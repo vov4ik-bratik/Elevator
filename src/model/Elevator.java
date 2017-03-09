b1804f30f8882076748d633e466f4b87217a9d28
vs <vov4ik.bratik@gmail.com>
2017-03-09 03:28:16 +0200

    models creating

--- src/model/Elevator.java
+++ src/model/Elevator.java
@@ -1,12 +1,44 @@
 package model;
 
+import constant.Const;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vs on 08.03.17.
  */
 public class Elevator extends MovingObjects {
 
-    private int capacity;
+    private final int capacity;
     private int currentLoad;
+
+    private List<Passenger> elevatedPassenger;
+
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+
+    public int getCurrentLoad() {
+        return currentLoad;
+    }
+
+    public void setCurrentLoad(int currentLoad) {
+        this.currentLoad = currentLoad;
+    }
+
+
+    public Elevator() {
+
+        currentLoad = 0;
+
+        //create a proper constructor which can choose elevator type and set capacity 
+        capacity = Const.PASSENGER_ELEVATOR_CAPACITY;
+
+        elevatedPassenger = new ArrayList<>();
+    }
 
     public void openDoors(){
         try {
